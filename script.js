b6c1dc689080f3424f3b9cb65afea50ad364ef0e
AJAY VENKATESH BABU <ajayvengateshbabu@gmail.com>
2024-09-15 20:23:48 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getFormvalue() {
     //Write your code here
-	const fName = document.getElementById("fname");
-	const lName = document.getELementById("lname");
+	const fName = document.getElementByName("fname");
+	const lName = document.getELementByName("lname");
 	alert(fName.value + " " +lName.value);
 
 }
